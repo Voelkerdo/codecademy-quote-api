579f2c86fa5cde0fa2a0676199f7089404fd8397
Workstation <you@example.com>
2021-09-28 18:28:52 +0200

    get random quote implemented

--- server.js
+++ server.js
@@ -8,3 +8,13 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
+  const randomQuote = {};
+  randomQuote.quote = getRandomElement(quotes);
+  res.status(200).send(randomQuote);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is now up and running!`);
+})
+
