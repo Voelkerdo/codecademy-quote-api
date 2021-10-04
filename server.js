d4ea62a378f36736ca2da2f0baef6c65ccaa0340
Workstation <you@example.com>
2021-10-04 18:58:01 +0200

    Add route with param person and error handler

--- server.js
+++ server.js
@@ -34,6 +34,22 @@ app.get('/api/quotes', (req, res, next) => {
   }
 });
 
+//My route with person as route parameter
+app.get('/api/quotes/:person', (req, res, next) => {
+  const person = req.params.person;
+  const quote = quotes.find(quote => quote.person === person);
+  if (quote) {
+    res.status(200).json(quote);
+  } else {
+    try {
+      throw new Error(`Error: No quote found for person ${person}`);
+    } catch (err) {
+      err.status = 404;
+      next(err);
+    }
+  }
+});
+
 app.post('/api/quotes', (req, res, next) => {
   const query = req.query;
   if (query.person && query.quote) {
@@ -42,6 +58,12 @@ app.post('/api/quotes', (req, res, next) => {
   } else {
     res.status(400).send();
   }
+});
+
+//Error Handling middleware
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(err.status).send(err.message);
 });
 
 app.listen(PORT, () => {
